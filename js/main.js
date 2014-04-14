0b3e12bb33b0346496b31894053df6beb2151227
juliangibellino <jgibelli@nycrzrjgibell.local>
2014-04-13 22:27:34 -0400

    Set up classes

--- js/main.js
+++ js/main.js
@@ -1,124 +1,70 @@
-//App
-var App = function(view, model){
+//MemoryBoardController
+var MemoryBoardController = function(view, model){
 	this.$el = $("#"+view);
 	this.model = model;
 	this.tiles = this.model.attributes.tiles;
-	this.gridColumns = this.model.attributes.gridColumns;
+	this.activeTiles = [];
 
 	this.init();
 };
 
-App.prototype = {
+MemoryBoardController.prototype = {
 	init: function(){
-		this.render();
+		this.events();
 		this.subscribe();
+		this.render();
 	},
 	subscribe: function(){
 		Events.broadcaster.on(Events.tileTurnedOver, $.proxy(this.compareTiles, this));
 	},
+	events: function(){
+		$("#reset", this.$el).on("click", $.proxy(this.resetBoard, this));
+	},
 	render: function(){
-		//loop through model to create board
+		//Loop through model to create board
 		for(var i=0; i < this.tiles.length; i++){
 			//create new tile and append it to grid
-			var tile = new Tile(this.tiles[i]);
-			this.$el.append(tile.$el);
+			var tile = new TileController(this.tiles[i]);
+			$("#tiles", this.$el).append(tile.$el);
 		}
 	},
-	compareTiles: function(){
-		var $activeTiles = $(".tile.active", this.$el);
-
-		if($activeTiles.length === 2){
-			isMatch = $activeTiles.eq(0).data("id") === $activeTiles.eq(1).data("id");
+	compareTiles: function(e, data){
+		//If less than 2 tiles are flipped, push this flipped tile to activeTiles array
+		if(this.activeTiles.length < 2) this.activeTiles.push(data.id);
+		
+		//If two tiles are flipped compare their id to see if they match
+		if(this.activeTiles.length === 2){
+			isMatch = this.activeTiles[0] === this.activeTiles[1];
 			if(isMatch) Events.broadcaster.trigger(Events.correctMatch);
 			else Events.broadcaster.trigger(Events.incorrectMatch);
+
+			//Trigger event that an attemp was made
+			Events.broadcaster.trigger(Events.attemp);
+
+			//Reset activeTiles
+			this.activeTiles.length = 0;
 		}
+	},
+	resetBoard: function(){
+		//Empty out tiles container
+		$("#tiles", this.$el).empty();
+		//Rerender tiles
+		this.render();
+		//Trigger event that the game has been reset
+		Events.broadcaster.trigger(Events.resetGame);
 	}
 };
 
-
-
-var Tile = function(data){
-	this.id = data.id;
-	this.letter = data.letter;
-	this.$el;
+/**
+ * Model of tiles for Memory Game
+ */
+var MemoryModel = function(data){
+	this.attributes = $.extend(true, {}, data);
 
 	this.init();
 };
 
-Tile.prototype = {
-	init: function(){
-		this.buildTile();
-		this.subscribe();
-		this.events();
-	},
-	subscribe: function(){
-		Events.broadcaster.on(Events.correctMatch, $.proxy(this.hideTile, this));
-		Events.broadcaster.on(Events.incorrectMatch, $.proxy(this.resetTile, this));
-	},
-	events: function(){
-		var self = this;
-		self.$el.on("click", $.proxy(self.flipTile, self));
-	},
-	buildTile: function(){
-		var html = '<div class="tile" data-id="' + this.id + '">'+ this.letter +'</div>';
-		this.$el = $(html);
-	},
-	flipTile: function(){
-		//check that tile hasn't been flipped and that no more than 2 tiles are already flipped
-		if(!this.$el.hasClass("active") && $(".tile.active").length < 2){
-			this.$el.addClass("active");
-			Events.broadcaster.trigger(Events.tileTurnedOver);
-		}
-	},
-	resetTile: function(){
-		var self = this;
-		if(this.$el.hasClass("active")){
-			setTimeout(function(){
-				self.$el.removeClass("active");
-			}, 1000);
-		}
-	},
-	hideTile: function(){
-		var self = this;
-		if(this.$el.hasClass("active")){
-			setTimeout(function(){
-				self.$el.removeClass("active");
-				self.$el.addClass("matched");
-			}, 1000);
-			
-		}
-	}
-};
-
-var Events = {
-	"tileTurnedOver": "tileTurnedOver",
-	"correctMatch": "correctMatch",
-	"incorrectMatch": "incorrectMatch",
-	"broadcaster": $(new Object())
-}
-
-
-var MemoryTiles = function(){
-	this.attributes = {
-		gridColumns: 2,
-		tiles: [
-			{
-				"id": "001",
-				"letter": "A",
-				"isMatched": false
-			},
-			{
-				"id": "002",
-				"letter": "B",
-				"isMatched": false
-			}
-		]
-	};
-
-	this.init();
-};
-
-MemoryTiles.prototype = {
+MemoryModel.prototype = {
 	init: function(){
 		this.duplicateTiles();
 		this.randomizeTiles();
@@ -133,7 +79,7 @@ MemoryTiles.prototype = {
 		var tiles = this.attributes.tiles,
 			tilesCopy = [];
 		
-		//make copy of tiles in model
+		//make a copy of the tiles in model
 		tiles.forEach(function(tile, index){
 			tilesCopy.push(tile);
 		});
@@ -164,7 +110,254 @@ MemoryTiles.prototype = {
 	}
 }
 
+/**
+ * Tile class for Memory Game.
+ * @param {Object} data The data field is a requred parameter used for providing information about the individual tile.
+ */
+var TileController = function(data){
+	this.id = data.id;
+	this.letter = data.letter;
+	this.$el;
+
+	this.init();
+};
+
+TileController.prototype = {
+	init: function(){
+		this.buildTile();
+		this.subscribe();
+		this.events();
+	},
+	/**
+	 * Events the tile subscribes to
+	 */
+	subscribe: function(){
+		Events.broadcaster.on(Events.correctMatch, $.proxy(this.hideTile, this));
+		Events.broadcaster.on(Events.incorrectMatch, $.proxy(this.resetTile, this));
+	},
+	/**
+	 * Dom events the tile listens for
+	 */
+	events: function(){
+		var self = this;
+		self.$el.on("click", $.proxy(self.flipTile, self));
+	},
+	/**
+	 * Build tile as a dom object
+	 * @return {$Object} A jQuery object of the dom element
+	 */
+	buildTile: function(){
+		var tileFront = '<div class="front"></div>',
+			tileBack = '<div class="back">'+ this.letter +'</div>',
+			tile = '<div class="tile">' + tileFront + tileBack + '</div>';
+		this.$el = $(tile);
+	},
+	/**
+	 * When a tile is clicked, flip it over to reveal its information
+	 */
+	flipTile: function(){
+		var id = this.id;
+
+		//check that tile hasn't been flipped and that no more than 2 tiles are already flipped
+		if(!this.$el.hasClass("active") && $(".tile.active").length < 2){
+			this.$el.addClass("active");
+			Events.broadcaster.trigger(Events.tileTurnedOver, {"id": id});
+		}
+	},
+	/**
+	 * When there is an incorrect match, reset flipped tile to its original state.
+	 */
+	resetTile: function(){
+		var self = this;
+
+		function reset(){
+			self.$el.removeClass("active");
+		};
+
+		if(this.$el.hasClass("active")) setTimeout(reset, 1000);
+	},
+	/**
+	 * When there is a correct match, hide flipped tile
+	 */
+	hideTile: function(){
+		var self = this;
+
+		function hide(){
+			self.$el.addClass("matched");
+			//self.$el.removeClass("active");
+		};
+
+		if(this.$el.hasClass("active")) setTimeout(hide, 1000);
+	}
+};
+
+/**
+ * Model for scoreBard
+ * @param {[type]} data [description]
+ */
+var ScoreBoardModel = function(data){
+	this.defaults = {
+		attemps: 0,
+		correct: 0,
+		score: 0,
+		totalOutcome: data.tiles.length
+	};
+	this.attributes = {};
+
+	this.init();
+};
+
+ScoreBoardModel.prototype = {
+	init: function(){
+		this.setDefaults();
+	},
+	set: function(attr, value){
+		this.attributes[attr] = value;
+		this.events.trigger('change');
+	},
+	get: function(attr){
+		return this.attributes[attr];
+	},
+	setDefaults: function(){
+		for(key in this.defaults){
+			this.attributes[key] = this.defaults[key];
+		}
+	},
+	events: $(new Object)
+};
+
+/**
+ * Score board controller
+ * @param {Object} model Required parameter to set model for controller
+ */
+var ScoreBoardController = function(view, model){
+	this.$el = $("#"+view);
+	this.model = model;
+
+	this.init();
+};
+
+ScoreBoardController.prototype = {
+	init: function(){
+		this.subscribe();
+	},
+	subscribe: function(){
+		Events.broadcaster.on(Events.attemp, $.proxy(this.updateAttemps, this));
+		Events.broadcaster.on(Events.correctMatch, $.proxy(this.updateCorrect, this));
+		Events.broadcaster.on(Events.resetGame, $.proxy(this.reset, this));
+	},
+	render: function(){
+		var score = this.model.get('score');
+		$("#score", this.$el).html('Congratulations! Your score is ' + score);
+	},
+	updateAttemps: function(){
+		console.log("updateAttemps");
+		var attemps = this.model.get('attemps');
+		this.model.set('attemps', attemps+1);
+
+		if(this.model.get('totalOutcome') === this.model.get('correct')){
+			this.updateScore();
+		}
+	},
+	updateCorrect: function(){
+		var correct = this.model.get('correct');
+		this.model.set('correct', correct+1);
+	},
+	updateScore: function(){
+		var attemps = this.model.get('attemps'),
+			correct = this.model.get('correct'),
+			wrong = attemps - correct,
+			totalOutcome = this.model.get('totalOutcome'),
+			efficiency = Math.round((totalOutcome * 100) / attemps) * 100;
+
+		//Set score
+		this.model.set('score', efficiency);
+		//Render scoreboard
+		this.render();
+	},
+	reset: function(){
+		this.model.setDefaults();
+	}
+}
+
+/**
+ * Event object used for publishing/subscribing
+ */
+var Events = {
+	"tileTurnedOver": "tileTurnedOver",
+	"correctMatch": "correctMatch",
+	"incorrectMatch": "incorrectMatch",
+	"attemp": "attemp",
+	"resetGame": "resetGame",
+	"broadcaster": $(new Object())
+};
+
+/**
+ * Memory data
+ */
+var memoryData = {
+	tiles: [
+		{
+			"id": "001",
+			"letter": "A"
+		},
+		{
+			"id": "002",
+			"letter": "B"
+		},
+		{
+			"id": "003",
+			"letter": "C"
+		},
+		{
+			"id": "004",
+			"letter": "D"
+		},
+		{
+			"id": "005",
+			"letter": "E"
+		},
+		{
+			"id": "006",
+			"letter": "F"
+		},
+		{
+			"id": "007",
+			"letter": "G"
+		},
+		{
+			"id": "008",
+			"letter": "H"
+		},
+		{
+			"id": "009",
+			"letter": "I"
+		},
+		{
+			"id": "010",
+			"letter": "J"
+		},
+		{
+			"id": "011",
+			"letter": "K"
+		},
+		{
+			"id": "012",
+			"letter": "L"
+		}
+	]
+}
+
+
+/**
+ * On document ready start up Memory Game
+ * @return {[type]} [description]
+ */
 $(function(){
-	var memoryTiles = new MemoryTiles();
-	var app = new App("memory-game", memoryTiles);
+	//Instantiate models for game
+	var memoryModel = new MemoryModel(memoryData);
+	var scoreBoardModel = new ScoreBoardModel(memoryData);
+	//Instantiate controller for game
+	var scoreBoardController = new ScoreBoardController("score-board", scoreBoardModel);
+	var memoryBoardController = new MemoryBoardController("memory-game", memoryModel);
 });
